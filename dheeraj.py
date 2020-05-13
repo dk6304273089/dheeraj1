b061f3c8daeecb53a5f10ef3149bf6062aea09db
dk6304273089 <65006527+dk6304273089@users.noreply.github.com>
2020-05-13 10:15:17 +0530

    Add files via upload

---
+++ dheeraj.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+1.
+
+
+# In[2]:
+
+
+a=[]
+b=(1,2,3,4,5,"dgh",'yui',5.6)
+for i in b:
+    a.append(i)
+
+
+# In[3]:
+
+
+a
+
+
+# In[ ]:
+
+
+2
+
+
+# In[4]:
+
+
+tuple=(1,2,3,"c",'c++')
+print(tuple[2])
+
+
+# In[ ]:
+
+
+3.
+
+
+# In[5]:
+
+
+dict={1:'dfg',2:'fgh',3:'hjk'}
+
+
+# In[7]:
+
+
+del dict[1]
+
+
+# In[8]:
+
+
+dict
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
